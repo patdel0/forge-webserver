307f31b6fd5ceee1fcfb80695fdc05b980e7d499
patdel0 <zefiru@protonmail.com>
2025-03-06 17:14:24 -0300

    Add 'quit' and 'exit' commands to close connection

--- echo-server.ts
+++ echo-server.ts
@@ -8,6 +8,11 @@ server.listen({ host: '127.0.0.1', port: 2025 })
 function handleNewConnection(socket: Socket): void {
     const echoData = (data: Buffer): void => {
         socket.write(data)
+
+        if (data.includes('quit') || data.includes('exit')) {
+            socket.write('Goodbye! :3 \n')
+            socket.end()
+        }
     }
     socket.on("data", echoData);
 }
