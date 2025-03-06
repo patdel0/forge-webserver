9551d2e040bbcd1f2722f70672009b67ede6c119
patdel0 <zefiru@protonmail.com>
2025-03-06 13:14:26 -0300

    Add new connection handler to TCP echo server

--- echo-server.ts
+++ echo-server.ts
@@ -1,5 +1,16 @@
 import { createServer, Socket } from 'net'
 
 const server = createServer()
+server.on('connection', handleNewConnection)
 server.on('error', (e: Error) => { throw e })
 server.listen({ host: '127.0.0.1', port: 2025 })
+
+function handleNewConnection(socket: Socket): void {
+    console.log(
+        'new connection', {
+            socket,
+            address: socket.remoteAddress,
+            port: socket.remotePort
+        }
+    )
+}
